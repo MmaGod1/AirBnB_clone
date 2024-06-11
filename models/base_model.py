c62cb653192e75d28df525b84555357ddd28cea8
MmaGod1 <109483181+MmaGod1@users.noreply.github.com>
2024-06-11 21:58:42 +0100

    Import the uuid

--- models/base_model.py
+++ models/base_model.py
@@ -3,6 +3,11 @@
 import models
 from uuid import uuid4
 
+
 class BaseModel:
+  """Represents the parent class gor all other classes for the AirBnB console project."""
+  
   def __init__(self):
+    """Initialize instances of BaseModel."""
+    self.id = str(uuid4())
     
