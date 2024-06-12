6cca28f8573d6fdbaff033005d6374f6a94a5242
MmaGod1 <109483181+MmaGod1@users.noreply.github.com>
2024-06-12 13:10:47 +0100

    Update more functionality of the project

--- models/base_model.py
+++ models/base_model.py
@@ -1,13 +1,20 @@
 #!/usr/bin/python3
 """Defines the BaseModel class."""
-import models
 from uuid import uuid4
-
+from datetime import datetime
 
 class BaseModel:
-  """Represents the parent class gor all other classes for the AirBnB console project."""
+  """Represents the parent class for all other classes for the AirBnB console project."""
   
   def __init__(self):
     """Initialize instances of BaseModel."""
     self.id = str(uuid4())
-    
+    self.created_at = datetime.now()
+    self.updated_at = datetime.now()
+  def save(self):
+    """updates the updated_at attribute with the current datetime"""
+    self.updated_at = datetime.now()
+
+mytest = BaseModel()
+print(mytest.id)
+print(mytest.created_at)
