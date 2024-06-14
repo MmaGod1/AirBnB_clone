aa032f6afa01652f3056a92d36530206f483018e
MmaGod1 <gmmaduchukwu@gmail.com>
2024-06-14 14:11:44 -0700

    identation issues fix

--- models/base_model.py
+++ models/base_model.py
@@ -12,8 +12,8 @@ class BaseModel:
             for key, value in kwargs.items():
                 if key == 'created_at' or key == 'updated_at':
                     value = datetime.fromisoformat(value)
-                    if key != '__class__':
-                        setattr(self, key, value)
+                if key != '__class__':
+                    setattr(self, key, value)
         else:
             self.id = str(uuid4())
             self.created_at = datetime.now()
