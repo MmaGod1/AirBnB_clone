8250b88b81340c9287441d0f8106eb8f42e9ad78
MmaGod1 <gmmaduchukwu@gmail.com>
2024-06-14 12:49:36 -0700

    pycode check

--- models/base_model.py
+++ models/base_model.py
@@ -3,30 +3,34 @@
 from uuid import uuid4
 from datetime import datetime
 
+
 class BaseModel:
-  """Represents the parent class for all other classes for the AirBnB console project."""
-  def __init__(self):
-    """Initialize instances of BaseModel."""
-    self.id = str(uuid4())
-    self.created_at = datetime.now()
-    self.updated_at = datetime.now()
+    """Represents the parent model for all other model for this project."""
+    def __init__(self):
+        """Initialize instances of BaseModel."""
+        self.id = str(uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
 
-  def save(self):
-    """updates the updated_at attribute with the current datetime"""
-    self.updated_at = datetime.now()
+    def save(self):
+        """updates the updated_at attribute with the current datetime"""
+        self.updated_at = datetime.now()
 
-  def to_dict(self):
-    """Returns a dictionary of the BaseModel
-    
-    the dictionary includes the key-value pair, and a __class__ key indicating the class name.
-    """
-    instance_dict = self.__dict__.copy()
-    instance_dict['__class__'] = self.__class__.__name__
-    instance_dict['created_at'] = self.created_at.isoformat(timespec='microseconds')
-    instance_dict['updated_at'] = self.updated_at.isoformat(timespec='microseconds')
-    return instance_dict
+    def to_dict(self):
+        """Returns a dictionary of the BaseModel
 
-  def __str__(self):
-    """Return the str representation of the BaseModel instance."""
-    class_name = self.__class__.__name__
-    return f"[{classname}] ({self.id}) {self.__dict__}"
+        the dictionary includes the key-value pair, and
+        a __class__ key indicating the class name.
+        """
+        inst_dict = self.__dict__.copy()
+        inst_dict['__class__'] = self.__class__.__name__
+        inst_dict['created_at'] = self.created_at.isoformat(
+                timespec='microseconds')
+        inst_dict['updated_at'] = self.updated_at.isoformat(
+                timespec='microseconds')
+        return inst_dict
+
+    def __str__(self):
+        """Return the str representation of the BaseModel instance."""
+        class_name = self.__class__.__name__
+        return f"[{classname}] ({self.id}) {self.__dict__}"
