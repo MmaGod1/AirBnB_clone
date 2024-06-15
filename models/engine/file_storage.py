0328a8e8748a925724e73aab852e0c77da7d4f57
MmaGod1 <109483181+MmaGod1@users.noreply.github.com>
2024-06-15 13:22:25 +0100

    Handle serialization and deserialization properly

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 """Serializes instances to a JSON file and deserializes JSON to instances"""
 import json
-
+from models.base_model import BaseModel
 
 class FileStorage:
-    """Serializing and deserialing objects to and from a JSON file."""
+    """Serializing and deserializing objects to and from a JSON file."""
 
     __file_path: str = "file.json"
     __objects: dict = {}
@@ -23,12 +23,17 @@ class FileStorage:
     def save(self):
         """Serializes the objects dictionary to a JSON file."""
         with open(self.__file_path, "w") as f:
-            json.dump(self.__objects, f, indent=4)
+            obj_dict = {key: obj.to_dict() for key, obj in self.__objects.items()}
+            json.dump(obj_dict, f, indent=4)
 
     def reload(self):
         """Deserializes the JSON file to the objects dict if it exists."""
         try:
             with open(self.__file_path, "r") as f:
-                self.__objects = json.load(f)
+                obj_dict = json.load(f)
+                for key, value in obj_dict.items():
+                    cls_name, obj_id = key.split(".")
+                    if cls_name == "BaseModel":
+                        self.__objects[key] = BaseModel(**value)
         except FileNotFoundError:
             pass
