2213217b7c55af2fcd7937723e920451645d373a
MmaGod1 <109483181+MmaGod1@users.noreply.github.com>
2024-06-15 18:03:15 +0100

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -19,12 +19,12 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            storage.new(self)
+            models.storage.new(self)
 
     def save(self):
         """Updates the updated_at attribute with the current datetime."""
         self.updated_at = datetime.now()
-        storage.save()
+        models.storage.save()
 
     def to_dict(self):
         """Returns a dictionary containing all keys/values of __dict__ of the instance."""
