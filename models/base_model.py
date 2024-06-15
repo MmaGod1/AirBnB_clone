dab4e9dd32517dd6176dbe9e734fd082ebd0dc3f
MmaGod1 <109483181+MmaGod1@users.noreply.github.com>
2024-06-15 17:40:26 +0100

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 """Defines the BaseModel class."""
-from models import storage
-from uuid import uuid4
+import uuid
 from datetime import datetime
+from models import storage
 
 
 class BaseModel:
-    """Represents the parent model for all other model for this project."""
+    """Represents the parent model for all other models for this project."""
+
     def __init__(self, *args, **kwargs):
         """Initialize instances of BaseModel."""
         if kwargs:
@@ -15,30 +16,24 @@ class BaseModel:
                     value = datetime.fromisoformat(value)
                 setattr(self, key, value)
         else:
-            self.id = str(uuid4())
+            self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
             storage.new(self)
 
     def save(self):
-        """updates the updated_at attribute with the current datetime"""
+        """Updates the updated_at attribute with the current datetime."""
         self.updated_at = datetime.now()
-        storage.save(self)
+        storage.save()
 
     def to_dict(self):
-        """Returns a dictionary of the BaseModel
-
-        the dictionary includes the key-value pair, and
-        a __class__ key indicating the class name.
-        """
-        micro = 'microseconds'
+        """Returns a dictionary containing all keys/values of __dict__ of the instance."""
         inst_dict = self.__dict__.copy()
         inst_dict['__class__'] = self.__class__.__name__
-        inst_dict['created_at'] = self.created_at.isoformat(timespec=micro)
-        inst_dict['updated_at'] = self.updated_at.isoformat(timespec=micro)
+        inst_dict['created_at'] = self.created_at.isoformat()
+        inst_dict['updated_at'] = self.updated_at.isoformat()
         return inst_dict
 
     def __str__(self):
         """Return the str representation of the BaseModel instance."""
-        class_name = self.__class__.__name__
-        return "[{}] ({}) {}".format(class_name, self.id, self.__dict__)
+        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
