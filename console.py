41c9b5d4af1fdb6f0866b1d3bc851e46178ed055
MmaGod1 <109483181+MmaGod1@users.noreply.github.com>
2024-06-15 22:16:18 +0100

    Update console.py

--- console.py
+++ console.py
@@ -6,16 +6,11 @@ import shlex
 from models.base_model import BaseModel
 from models import storage
 
-
 class HBNBCommand(cmd.Cmd):
     prompt = '(hbnb) '
 
     def do_create(self, arg):
-        """Creates a new BaseModel instance.
-
-        Args:
-        arg (str): The class name (optional).
-        """
+        """Creates a new BaseModel instance."""
         if not arg:
             print("** class name missing **")
             return
@@ -28,8 +23,7 @@ class HBNBCommand(cmd.Cmd):
             print("** class doesn't exist **")
 
     def do_show(self, arg):
-        """Prints the string representation of an
-        instance based on the class name and id."""
+        """Prints the string representation of an instance based on the class name and id."""
         if not arg:
             print("** class name missing **")
             return
@@ -53,39 +47,37 @@ class HBNBCommand(cmd.Cmd):
             print("** no instance found **")
 
     def do_destroy(self, arg):
-        """Deletes an instance based on the class name and
-        id, updates JSON file."""
+        """Deletes an instance based on the class name and id, updates JSON file."""
         if not arg:
             print("** class name missing **")
             return
-
+        
         args = shlex.split(arg)
         if len(args) < 1:
             print("** class name missing **")
             return
-
+        
         class_name = args[0]
         if class_name not in storage_classes:
             print("** class doesn't exist **")
             return
-
+        
         if len(args) < 2:
             print("** instance id missing **")
             return
-
+        
         instance_id = args[1]
         key = "{}.{}".format(class_name, instance_id)
-
+        
         if key not in storage.all():
             print("** no instance found **")
             return
-
-    del storage.all()[key]
-    storage.save()
+        
+        del storage.all()[key]
+        storage.save()
 
     def do_all(self, arg):
-        """Prints all string representations of all
-        instances based on class name."""
+        """Prints all string representations of all instances based on class name."""
         args = shlex.split(arg)
         if not arg or args[0] == "":
             all_instances = [str(obj) for obj in storage.all().values()]
@@ -101,8 +93,7 @@ class HBNBCommand(cmd.Cmd):
             print(all_instances)
 
     def do_update(self, arg):
-        """Updates an instance based on the class name and
-        id by adding or updating attribute."""
+        """Updates an instance based on the class name and id by adding or updating attribute."""
         if not arg:
             print("** class name missing **")
             return
@@ -133,18 +124,15 @@ class HBNBCommand(cmd.Cmd):
             return
 
         attribute_name = args[2]
-        attribute_value = args[3]
+        attribute_value_str = args[3]
 
-        obj = storage.all()[key]
-
-        # Try to convert attribute_value to the correct type
         try:
-            attribute_value = eval(attribute_value)
+            attribute_value = eval(attribute_value_str)
         except (NameError, SyntaxError):
-            pass
-
-        setattr(obj, attribute_name, attribute_value)
-        obj.save()
+            attribute_value = attribute_value_str
+        
+        setattr(storage.all()[key], attribute_name, attribute_value)
+        storage.all()[key].save()
 
     def do_EOF(self, arg):
         """Handles EOF (Ctrl+D) signal."""
@@ -154,7 +142,6 @@ class HBNBCommand(cmd.Cmd):
     def do_quit(self, arg):
         """Quit command to exit the program."""
         return True
-
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
