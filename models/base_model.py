95557270a327e2ea4264511bc438d7068dc4739b
MmaGod1 <109483181+MmaGod1@users.noreply.github.com>
2024-06-15 18:40:44 +0100

    Handle __class__

--- models/base_model.py
+++ models/base_model.py
@@ -12,9 +12,10 @@ class BaseModel:
         """Initialize instances of BaseModel."""
         if kwargs:
             for key, value in kwargs.items():
-                if key == 'created_at' or key == 'updated_at':
+                if key == "created_at" or key == "updated_at":
                     value = datetime.fromisoformat(value)
-                setattr(self, key, value)
+                if kwargs != "__class__":
+                    setattr(self, key, value)
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
@@ -29,9 +30,9 @@ class BaseModel:
     def to_dict(self):
         """Returns a dictionary containing all keys/values of __dict__ of the instance."""
         inst_dict = self.__dict__.copy()
-        inst_dict['__class__'] = self.__class__.__name__
-        inst_dict['created_at'] = self.created_at.isoformat()
-        inst_dict['updated_at'] = self.updated_at.isoformat()
+        inst_dict["__class__"] = self.__class__.__name__
+        inst_dict["created_at"] = self.created_at.isoformat()
+        inst_dict["updated_at"] = self.updated_at.isoformat()
         return inst_dict
 
     def __str__(self):
