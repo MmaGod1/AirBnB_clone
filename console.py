464b6792b2223bfa9bfe58c5da90fc719541eb93
MmaGod1 <gmmaduchukwu@gmail.com>
2024-06-15 12:25:06 -0700

    executable and indentation fix

--- console.py
+++ console.py
@@ -4,21 +4,22 @@ import cmd
 
 
 class HBNBCommand(cmd.Cmd):
-  """Defines the HolbertonBnB command interpreter."""
-  prompt = "(hbnb) "
+    """Defines the HolbertonBnB command interpreter."""
+    prompt = "(hbnb) "
 
-  def do_quit(self, line):
-    """quits command to exit the program"""
-    return True
-    
-  def do_EOF(self, arg):
-    """EOF command to exit the program"""
-    print("")
-    return True
+    def do_quit(self, line):
+        """quits command to exit the program"""
+        return True
 
- def emptyline(self):
-   """Do nothing on empty input line"""
-   pass
-  
+    def do_EOF(self, arg):
+        """EOF command to exit the program"""
+        print("")
+        return True
+
+    def emptyline(self):
+        """Do nothing on empty input line"""
+        pass
+
+
 if __name__ == '__main__':
-  HBNBCommand().cmdloop()
+    HBNBCommand().cmdloop()
