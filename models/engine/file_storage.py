5b9081bad06d4d089eb3eae099da66e775b78e37
MmaGod1 <109483181+MmaGod1@users.noreply.github.com>
2024-06-15 17:37:42 +0100

    Update file_storage.py

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
-"""Serializes instances to a JSON file and deserializes JSON to instances"""
+"""Serializes instances to a JSON file and deserializes JSON to instances."""
 import json
+import os
 from models.base_model import BaseModel
 
 class FileStorage:
     """Serializing and deserializing objects to and from a JSON file."""
 
-    __file_path: str = "file.json"
-    __objects: dict = {}
+    __file_path = "file.json"
+    __objects = {}
 
     def all(self):
         """Returns the dictionary containing all stored objects."""
@@ -15,25 +16,21 @@ class FileStorage:
 
     def new(self, obj):
         """Adds a new object to the storage dictionary."""
-        cls_name = obj.__class__.__name__
-        obj_id = obj.id
-        key = f"{cls_name}.{obj_id}"
+        key = f"{obj.__class__.__name__}.{obj.id}"
         self.__objects[key] = obj
 
     def save(self):
         """Serializes the objects dictionary to a JSON file."""
         with open(self.__file_path, "w") as f:
             obj_dict = {key: obj.to_dict() for key, obj in self.__objects.items()}
-            json.dump(obj_dict, f, indent=4)
+            json.dump(obj_dict, f)
 
     def reload(self):
-        """Deserializes the JSON file to the objects dict if it exists."""
-        try:
+        """Deserializes the JSON file to the objects dictionary if it exists."""
+        if os.path.exists(self.__file_path):
             with open(self.__file_path, "r") as f:
                 obj_dict = json.load(f)
                 for key, value in obj_dict.items():
                     cls_name, obj_id = key.split(".")
                     if cls_name == "BaseModel":
                         self.__objects[key] = BaseModel(**value)
-        except FileNotFoundError:
-            pass
