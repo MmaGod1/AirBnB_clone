eaba30cad013df9df0633072cabf255a8ef721f7
MmaGod1 <109483181+MmaGod1@users.noreply.github.com>
2024-06-15 11:30:20 +0100

    Update _init__.py

--- models/_init__.py
+++ models/_init__.py
@@ -1,1 +1,10 @@
+#!/usr/bin/python3
+"""Create a unique FileStorage instance for your application"""
+from models.engine import file_storage
+
+# Create a unique FileStorage instance
+storage = file_storage.FileStorage()
+
+# Call reload() method on the storage variable
+storage.reload()
 
