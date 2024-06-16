7926c09bb36f60f1ad75671691307adbd4104dff
MmaGod1 <109483181+MmaGod1@users.noreply.github.com>
2024-06-16 15:26:08 +0100

    Refining FileStorage class

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -13,27 +13,27 @@ class FileStorage:
 
     def all(self):
         """Returns the dictionary containing all stored objects."""
-        return self.__objects
+        return FileStorage.__objects
 
     def new(self, obj):
         """Adds a new object to the storage dictionary."""
-        key = f"{obj.__class__.__name__}.{obj.id}"
-        self.__objects[key] = obj
+        key = "{}.{}".format(obj.__class__.__name__, obj.id)
+        FileStorage.__objects[key] = obj
 
     def save(self):
         """Serializes the objects dictionary to a JSON file."""
-        with open(self.__file_path, "w") as f:
-            obj_items = self.__objects.items()
-            obj_dict = {key: obj.to_dict() for key, obj in obj_items}
+        obj_dict = {key: obj.to_dict() for key, obj in FileStorage.__objects.items()}
+        with open(FileStorage.__file_path, "w") as f:
             json.dump(obj_dict, f)
 
     def reload(self):
         """Deserializes the JSON file to the objects dictionary if it exists"""
-        if os.path.exists(self.__file_path):
-            with open(self.__file_path, "r") as f:
+        try:
+            with open(FileStorage.__file_path, "r") as f:
                 obj_dict = json.load(f)
                 for key, value in obj_dict.items():
-                    cls_name, obj_id = key.split(".")
-                    if cls_name == "BaseModel":
-                        value.pop('__class__', None)
-                        self.__objects[key] = BaseModel(**value)
+                    cls_name = value['__class__']
+                    del value['__class__']
+                    self.new(eval(cls_name)(**value))
+        except FileNotFoundError:
+            return
