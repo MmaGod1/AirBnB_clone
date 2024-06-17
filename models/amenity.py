c7bc2aca2294e453f6183a121817b87a6279e435
MmaGod1 <gmmaduchukwu@gmail.com>
2024-06-17 05:49:25 -0700

    pep8 check and executable files

---
+++ models/amenity.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Definesthe Amenity class"""
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """Represents the amenities
+
+    Attributes:
+        name (str): The name of the Amenity.
+    """
+    name = ""
